7c049f51010fb3ea49e7b9df816361970ae39ab2
afd <afd@alup5.academiapostal.es.local>
2020-07-14 19:29:50 +0200

    dashboard muestra fechas

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -1,15 +1,26 @@
 const { Usuarios, Tareas} = require("../models");
+const moment = require("moment");
 
 function dashboard(req, res){
     const usuarios= req.session.usuarios;
+
     Usuarios.findByPk(usuarios.id, {
         include: {model: Tareas, as: 'tareas'}
     })
+
     .then(usuarios => {
-        const tareas = usuarios.tareas;
-        res.render('dashboard', {usuarios, tareas})
-    })
+        const tareas = usuarios.tareas.map(tareas => {
+            return {
+                nombre: tareas.nombre,
+                fec_ini: moment(tareas.fec_ini).format("DD/MM/YYYY"),
+                fec_venc: tareas.fec_venc && moment(tareas.fec_venc).format("DD/MM/YYYY")
+            }
+        });
     
+        res.render('dashboard', {usuarios, tareas})
+
+    })
+
 }
 
 
