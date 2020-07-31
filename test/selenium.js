5e2d54f3087a1b5cbbd3bfd91d01d25ee36a624b
afd <afd@alup5.academiapostal.es.local>
2020-07-31 18:29:08 +0200

    cambion selenium

--- test/selenium.js
+++ test/selenium.js
@@ -4,7 +4,7 @@ const { doesNotMatch } = require('assert');
 
 describe("Formulario de login", function () {
 
-    it("Inicia sesion con exito", function () {
+    it("Inicia sesion con exito", async function () {
         this.timeout(10000);
 
         let driver = await new Builder().forBrowser('chrome').build();
@@ -24,7 +24,33 @@ describe("Formulario de login", function () {
 
         }
         finally {
-            driver.quit();
+           await driver.quit();
+        }
+
+    })
+
+
+    it("Inicia sesion con exito", async function () {
+        this.timeout(10000);
+
+        let driver = await new Builder().forBrowser('chrome').build();
+        try {
+            // Navigate to Url
+            await driver.get('http://www.localhost:3000/login');
+
+            //Introduce email, contraseña y pulsa enter
+            await driver.findElement(By.name('email')).sendKeys('armm@gmail.com');
+            await driver.findElement(By.name('password')).sendKeys('dadadaa', Key.ENTER);
+
+            let firstResult = await driver.wait(until.elementLocated(By.css('h1')), 10000);
+
+            //console.log(await firstResult.getAttribute('textContent'));
+            assert.equal("Bienvenido, Usuario", await firstResult.getAttribute('textContent'));
+
+
+        }
+        finally {
+           await driver.quit();
         }
 
     })
